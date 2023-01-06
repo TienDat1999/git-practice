bf794808e528f66bed8bbcba1d2232222f606db4
TienDat1999 <letiendat1999py@gmail.com>
2023-01-06 16:21:31 +0700

    caculate all function

--- fuction.tsx
+++ fuction.tsx
@@ -1,3 +1,16 @@
 const calculateAdding = (a: number, b:number) => {
         return a + b;
 }
+
+const calculateRemoving = (a: number, b:number) => {
+    return a - b;
+}
+
+const calculateMultiple = (a: number, b:number) => {
+    const c = a * b;
+    return c;
+}
+
+const divideCalculate = (a: number, b:number) => {
+    return a / b
+}
