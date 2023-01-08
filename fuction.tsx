43ab5646d21678007e6b4db5fa3bd9dc30f65feb
TienDat1999 <letiendat1999py@gmail.com>
2023-01-08 22:39:36 +0700

    f3

--- fuction.tsx
+++ fuction.tsx
@@ -6,10 +6,10 @@ const calculateRemoving = (a: number, b:number) => {
     return a - b;
 }
 
-// const calculateMultiple = (a: number, b:number) => {
-//     const c = a * b;
-//     return c;
-// }
+const calculateMultiple = (a: number, b:number) => {
+    const c = a * b;
+    return c;
+}
 
 // const divideCalculate = (a: number, b:number) => {
 //     return a / b
